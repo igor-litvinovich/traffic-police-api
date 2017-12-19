e454c1ba2757524383bd5039dace97f074750de8
aleksey.kuchik <aleksey.kuchik@itechart-group.com>
2017-12-19 18:00:11 +0300

    enabled lazy load no tran

--- src/main/java/rest/configs/DatabaseConfig.java
+++ src/main/java/rest/configs/DatabaseConfig.java
@@ -58,6 +58,7 @@ public class DatabaseConfig {
     hibernateProperties.put("hibernate.dialect", HIBERNATE_DIALECT);
     hibernateProperties.put("hibernate.show_sql", HIBERNATE_SHOW_SQL);
     hibernateProperties.put("hibernate.hbm2ddl.auto", HIBERNATE_HBM2DDL_AUTO);
+    hibernateProperties.put("hibernate.enable_lazy_load_no_trans", true);
     sessionFactoryBean.setHibernateProperties(hibernateProperties);
     return sessionFactoryBean;
   }
