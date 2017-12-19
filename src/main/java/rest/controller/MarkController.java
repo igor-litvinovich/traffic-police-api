402d142eee7649699ee63693636b1ed1e35309ba
aleksey.kuchik <aleksey.kuchik@itechart-group.com>
2017-12-19 17:00:30 +0300

    fixed mark mapping

--- src/main/java/rest/controller/MarkController.java
+++ src/main/java/rest/controller/MarkController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import rest.entity.FirmsEntity;
+import rest.entity.JackedCarsEntity;
 import rest.entity.MarksEntity;
 import rest.request.RequestParams;
 import rest.service.MarkServiceImpl;
@@ -21,19 +22,9 @@ public class MarkController extends BaseController<MarksEntity> {
     @Autowired
     private MarkServiceImpl service;
 
-    @Override
-    public ResponseEntity getAll(@ModelAttribute() RequestParams requestParams, Class<MarksEntity> tClass) {
-        List<MarksEntity> entities = service.filterEntities(requestParams, MarksEntity.class);
-        Map result = new HashMap();
-        Map resultEntities = new HashMap();
-        resultEntities.put("id", entities.get(0).getId());
-        resultEntities.put("markName", entities.get(0).getMarkName());
-        List<Map> list = new ArrayList();
-        list.add(resultEntities);
-        result.put("data", list);
-        result.put("recordsTotal", entities.size());
-        result.put("recordsFiltered", entities.size());
-        return new ResponseEntity(result, HttpStatus.OK);
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public ResponseEntity getAll(@ModelAttribute() RequestParams requestParams) {
+        return super.getAll(requestParams, MarksEntity.class);
     }
 
     @RequestMapping(value = "", method = RequestMethod.DELETE)
